9e924eada3e44820d1a00f48fcf40931dfec903c
roiesh <roieshalom@users.noreply.github.com>
2025-03-20 20:12:31 +0100

    Fixed base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/fragen-katalog/",  // ✅ This must match your GitHub Pages repo name
+  base: "",  // ✅ Set to an empty string for custom domains
   plugins: [react()],
 });
