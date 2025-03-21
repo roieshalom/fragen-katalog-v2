52c0208eec3712b615edeb04401e9f2524916fd7
roiesh <roieshalom@users.noreply.github.com>
2025-03-22 00:39:58 +0100

    sync before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/fragen-katalog/', // 👈 must match your GitHub repo name
+  base: '/fragen-katalog/', // ✅ must match repo name
   plugins: [react()],
-});
+})
