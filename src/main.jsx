9957ec9f4ebad328dc1bdb5c135e36d022048045
roiesh <roieshalom@users.noreply.github.com>
2025-04-10 17:11:39 +0200

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+if (import.meta.env.PROD) {
+  (function(c, l, a, r, i, t, y) {
+    c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1;
+    t.src = "https://www.clarity.ms/tag/qyg3chkjg3";
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+  })(window, document, "clarity", "script");
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
