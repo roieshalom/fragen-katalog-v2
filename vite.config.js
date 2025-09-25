010bf7829783479c740606a3fb21ab4a9c015517
roiesh <roieshalom@users.noreply.github.com>
2025-09-25 22:54:07 +0200

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/", // This is fine for GitHub Pages if project is at root of the user/organization site
+  base: "/fragen-katalog-v2/", // This is fine for GitHub Pages if project is at root of the user/organization site
   plugins: [react()],
   build: {
     outDir: 'docs' // <-- Change this from 'dist' to 'docs' for GitHub Pages compatibility
