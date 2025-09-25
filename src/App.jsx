81e73ff4e145d8e5cc615df4fd221638e4021787
roiesh <roieshalom@users.noreply.github.com>
2025-09-25 23:33:13 +0200

    fix fetch path app jsx2

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
       }
     }, 100);
 
-    fetch("public/data/questions.json")
+    fetch(`${import.meta.env.BASE_URL}data/questions.json`)
       .then((res) => res.json())
       .then((data) => {
         const formatted = data.map((item) => {
